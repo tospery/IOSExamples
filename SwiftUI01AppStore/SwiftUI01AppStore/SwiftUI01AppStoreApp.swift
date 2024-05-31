d98141901fd9c2f97fed87bb89320d033cfa88a2
tospery <tospery@gmail.com>
2024-05-31 11:37:33 +0800

    SwiftUI01AppStore

---
+++ SwiftUI01AppStore/SwiftUI01AppStore/SwiftUI01AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI01AppStoreApp.swift
+//  SwiftUI01AppStore
+//
+//  Created by 杨建祥 on 2024/5/31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI01AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
