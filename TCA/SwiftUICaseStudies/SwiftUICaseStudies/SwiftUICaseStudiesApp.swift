9ce704b8bffd570b6a85fe1449c5da6f2ba23874
tospery <tospery@gmail.com>
2024-06-01 19:07:36 +0800

    SwiftUICaseStudies

---
+++ TCA/SwiftUICaseStudies/SwiftUICaseStudies/SwiftUICaseStudiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICaseStudiesApp.swift
+//  SwiftUICaseStudies
+//
+//  Created by 杨建祥 on 2024/6/1.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICaseStudiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
